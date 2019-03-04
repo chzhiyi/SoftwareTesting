210d14aa8bb5db579abd299cd5e9014bc45b76ec
chzhiyi <258279548@qq.com>
2019-03-04 18:04:50 +0800

    Update ReserveIntoWarehouse.py

--- testPackage/operateExecl/ReserveIntoWarehouse.py
+++ testPackage/operateExecl/ReserveIntoWarehouse.py
@@ -8,13 +8,15 @@ book = xlrd.open_workbook("倒车入库-version-1(实际值).xlsx", logfile=sys.
 sheet = book.sheet_by_name("0308所有接口QPS")
 
 # 建立一个MySQL连接
-connection = pymysql.connect (host="127.0.0.1", port=6006, user = "root", passwd = "123456", db = "test",)
+connection = pymysql.connect (host="db.stg.helijia.com", port=6006, user = "hljtest_dev", passwd = "hljtest_dev123456", db = "hlj",)
 
 # 获得游标对象, 用于逐行遍历数据库数据
 cursor = connection.cursor()
 
 # 创建插入SQL语句
 query = """INSERT INTO save (instance_name, exp_190311, exp_190311_single, max181111, max180919, max180515, max180311, max171111, max170923, max170515, max170311) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+
+values = []
 
 # 创建一个for循环迭代读取xls文件每行数据的, 从第二行开始是要跳过标题
 for r in range(2, sheet.nrows):
@@ -30,9 +32,11 @@ for r in range(2, sheet.nrows):
       exp_190311  = sheet.cell(r,10).value if sheet.cell(r,10).value else "0"
       exp_190311_single = sheet.cell(r,14).value if sheet.cell(r,14).value else "0"
       print(instance_name,max181111,max180919,max180515,max180311,max171111,max170923,max170515,max170311,exp_190311 ,exp_190311_single)
-      values = (instance_name, exp_190311, exp_190311_single, max181111,max180919,max180515,max180311,max171111,max170923,max170515,max170311)
-      # 执行sql语句
-      cursor.execute(query, values)
+      value = (instance_name, exp_190311, exp_190311_single, max181111,max180919,max180515,max180311,max171111,max170923,max170515,max170311)
+      values.append(value)
+
+# 执行sql语句
+cursor.executemany(query, values)
 
 # 关闭游标
 cursor.close()
